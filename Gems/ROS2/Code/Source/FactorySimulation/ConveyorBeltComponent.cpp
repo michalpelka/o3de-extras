7c7155bf05b19f1a4970c69e19987967fbcda79d
Michał Pełka <michal.pelka@robotec.ai>
2023-06-20 09:56:27 +0200

    Velocity base conveyor
    Signed-off-by: Michał Pełka <michal.pelka@robotec.ai>

--- Gems/ROS2/Code/Source/FactorySimulation/ConveyorBeltComponent.cpp
+++ Gems/ROS2/Code/Source/FactorySimulation/ConveyorBeltComponent.cpp
@@ -7,6 +7,11 @@
  */
 #include "ConveyorBeltComponent.h"
 #include <AzCore/Serialization/EditContext.h>
+#include <AzFramework/Physics/Common/PhysicsSimulatedBody.h>
+#include <AzFramework/Physics/Components/SimulatedBodyComponentBus.h>
+#include <AzFramework/Physics/PhysicsSystem.h>
+#include <AzFramework/Physics/RigidBodyBus.h>
+#include <LmbrCentral/Shape/SplineComponentBus.h>
 #include <Source/RigidBodyComponent.h>
 
 namespace ROS2
@@ -27,11 +32,7 @@ namespace ROS2
                     ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
                     ->Attribute(AZ::Edit::Attributes::Category, "ROS2")
                     ->Attribute(AZ::Edit::Attributes::AppearsInAddComponentMenu, AZ_CRC_CE("Game"))
-                    ->DataElement(
-                        AZ::Edit::UIHandlers::Default,
-                        &ConveyorBeltComponent::m_speed,
-                        "Speed",
-                        "Speed of the conveyor belt")
+                    ->DataElement(AZ::Edit::UIHandlers::Default, &ConveyorBeltComponent::m_speed, "Speed", "Speed of the conveyor belt")
                     ->DataElement(
                         AZ::Edit::UIHandlers::EntityId,
                         &ConveyorBeltComponent::m_TempConveyorEntityId,
@@ -43,22 +44,129 @@ namespace ROS2
 
     void ConveyorBeltComponent::GetRequiredServices(AZ::ComponentDescriptor::DependencyArrayType& required)
     {
-        //        required.push_back(AZ_CRC_CE("PhysicsRigidBodyService"));
+        required.push_back(AZ_CRC_CE("SplineService"));
+        required.push_back(AZ_CRC_CE("PhysicsColliderService"));
     }
 
     void ConveyorBeltComponent::Activate()
     {
+        m_onTriggerEnterHandler = AzPhysics::SimulatedBodyEvents::OnTriggerEnter::Handler(
+            [&]([[maybe_unused]] AzPhysics::SimulatedBodyHandle bodyHandle, [[maybe_unused]] const AzPhysics::TriggerEvent& event)
+            {
+                AZ_Printf("ConveyorBeltComponent", "OnTriggerEnter");
+                auto boxId = event.m_otherBody->GetEntityId();
+                if (m_entitiesOnBelt.contains(boxId))
+                {
+                    m_entitiesOnBelt[boxId]++;
+                }
+                else
+                {
+                    m_entitiesOnBelt[boxId] = 1;
+                }
+            });
+
+        m_onTriggerExitHandler = AzPhysics::SimulatedBodyEvents::OnTriggerExit::Handler(
+            [&]([[maybe_unused]] AzPhysics::SimulatedBodyHandle bodyHandle, [[maybe_unused]] const AzPhysics::TriggerEvent& event)
+            {
+                AZ_Printf("ConveyorBeltComponent", "OnTriggerExit");
+                auto boxId = event.m_otherBody->GetEntityId();
+                if (m_entitiesOnBelt.contains(boxId))
+                {
+                    auto& count = m_entitiesOnBelt[boxId];
+                    count--;
+                    if (count <= 0)
+                    {
+                        m_entitiesOnBelt.erase(boxId);
+                    }
+                }
+            });
+
+        Physics::RigidBodyNotificationBus::Handler::BusConnect(GetEntityId());
         AZ::TickBus::Handler::BusConnect();
     }
 
     void ConveyorBeltComponent::Deactivate()
     {
+        m_onTriggerEnterHandler.Disconnect();
+        m_onTriggerExitHandler.Disconnect();
         AZ::TickBus::Handler::BusDisconnect();
+        Physics::RigidBodyNotificationBus::Handler::BusDisconnect();
     }
 
+    void ConveyorBeltComponent::OnPhysicsEnabled(const AZ::EntityId& entityId)
+    {
+        AZ_Printf("ConveyorBeltComponent", "OnPhysicsEnabled");
+
+        AzPhysics::SystemInterface* physicsSystem = AZ::Interface<AzPhysics::SystemInterface>::Get();
+        AZ_Assert(physicsSystem, "No physics system");
+
+        AzPhysics::SceneInterface* sceneInterface = AZ::Interface<AzPhysics::SceneInterface>::Get();
+        AZ_Assert(sceneInterface, "No scene intreface");
+
+        AzPhysics::SceneHandle defaultSceneHandle = sceneInterface->GetSceneHandle(AzPhysics::DefaultPhysicsSceneName);
+        AZ_Assert(defaultSceneHandle != AzPhysics::InvalidSceneHandle, "Invalid default physics scene handle");
+
+        if (auto* physicsSystem = AZ::Interface<AzPhysics::SystemInterface>::Get())
+        {
+            AZStd::pair<AzPhysics::SceneHandle, AzPhysics::SimulatedBodyHandle> foundBody =
+                physicsSystem->FindAttachedBodyHandleFromEntityId(entityId);
+            if (foundBody.first != AzPhysics::InvalidSceneHandle)
+            {
+                AzPhysics::SimulatedBodyEvents::RegisterOnTriggerEnterHandler(foundBody.first, foundBody.second, m_onTriggerEnterHandler);
+                AzPhysics::SimulatedBodyEvents::RegisterOnTriggerExitHandler(foundBody.first, foundBody.second, m_onTriggerExitHandler);
+            }
+        }
+
+        m_sceneFinishSimHandler = AzPhysics::SceneEvents::OnSceneSimulationFinishHandler(
+            [this]([[maybe_unused]] AzPhysics::SceneHandle sceneHandle, float fixedDeltatime)
+            {
+                this->PostPhysicsSubTick(fixedDeltatime);
+            },
+            aznumeric_cast<int32_t>(AzPhysics::SceneEvents::PhysicsStartFinishSimulationPriority::Components));
+        sceneInterface->RegisterSceneSimulationFinishHandler(defaultSceneHandle, m_sceneFinishSimHandler);
+    }
+    void ConveyorBeltComponent::OnPhysicsDisabled(const AZ::EntityId& entityId)
+    {
+        m_onTriggerEnterHandler.Disconnect();
+        m_onTriggerExitHandler.Disconnect();
+    }
+    void ConveyorBeltComponent::PostPhysicsSubTick(float fixedDeltaTime)
+    {
+        AZ::ConstSplinePtr splinePtr{ nullptr };
+        LmbrCentral::SplineComponentRequestBus::EventResult(splinePtr, m_entity->GetId(), &LmbrCentral::SplineComponentRequests::GetSpline);
+        AZ_Assert(splinePtr, "Spline pointer is null");
+
+        AZ::Vector3 conveyeredEntityPositionWorld{ 0 };
+        AZ::Quaternion conveyeredEntityRotationWorld{ AZ::Quaternion::CreateIdentity() };
+
+        for (auto& [m_TempConveyorEntityId, _] : m_entitiesOnBelt)
+        {
+            AZ::TransformBus::EventResult(
+                conveyeredEntityPositionWorld, m_TempConveyorEntityId, &AZ::TransformBus::Events::GetWorldTranslation);
+            AZ::TransformBus::EventResult(
+                conveyeredEntityRotationWorld, m_TempConveyorEntityId, &AZ::TransformBus::Events::GetWorldRotationQuaternion);
+
+            AZ::Transform splineTransform = AZ::Transform::Identity();
+            AZ::TransformBus::EventResult(splineTransform, m_entity->GetId(), &AZ::TransformBus::Events::GetWorldTM);
+            splineTransform.Invert();
+
+            const AZ::Vector3 conveyeredEntityPositionLocal = splineTransform.TransformPoint(conveyeredEntityPositionWorld);
+            const AZ::Quaternion conveyeredEntityRotationLocal = splineTransform.GetRotation() * conveyeredEntityRotationWorld;
+
+            const AZ::Vector3 boxDirection = conveyeredEntityRotationLocal.TransformVector(AZ::Vector3::CreateAxisX());
+            AZ::PositionSplineQueryResult r = splinePtr->GetNearestAddressPosition(conveyeredEntityPositionLocal);
+
+            AZ::Vector3 tangent = splinePtr->GetTangent(r.m_splineAddress);
+
+            Physics::RigidBodyRequestBus::Event(m_TempConveyorEntityId, &Physics::RigidBodyRequests::SetLinearVelocity, tangent * m_speed);
+
+            AZ::Vector3 rot = tangent.Cross(boxDirection);
+            Physics::RigidBodyRequestBus::Event(m_TempConveyorEntityId, &Physics::RigidBodyRequests::SetAngularVelocity, -rot * m_speed);
+        }
+    }
     void ConveyorBeltComponent::OnTick(float deltaTime, AZ::ScriptTimePoint time)
     {
-        AZ_Printf("ConveyorBeltComponent", "ConveyorBeltComponent::OnTick");
+
     }
 
 } // namespace ROS2
--- Gems/ROS2/Code/Source/FactorySimulation/ConveyorBeltComponent.h
+++ Gems/ROS2/Code/Source/FactorySimulation/ConveyorBeltComponent.h
@@ -7,12 +7,19 @@
 */
 #pragma once
 
+#include "AzCore/Math/Aabb.h"
+#include "AzCore/std/containers/unordered_map.h"
+#include "AzCore/std/containers/unordered_set.h"
+#include "AzFramework/Physics/Common/PhysicsTypes.h"
 #include <AzCore/Math/Matrix3x3.h>
 #include <AzCore/Math/Transform.h>
 #include <AzCore/Serialization/SerializeContext.h>
 #include <AzCore/Component/Component.h>
 #include <AzCore/Component/Entity.h>
 #include <AzCore/Component/TickBus.h>
+#include <AzFramework/Physics/Common/PhysicsEvents.h>
+#include <AzFramework/Physics/Common/PhysicsSimulatedBodyEvents.h>
+#include <AzFramework/Physics/RigidBodyBus.h>
 
 namespace ROS2
 {
@@ -20,7 +27,7 @@ namespace ROS2
    class ConveyorBeltComponent
         : public AZ::Component
         , public AZ::TickBus::Handler
-
+        , protected Physics::RigidBodyNotificationBus::Handler
    {
    public:
        AZ_COMPONENT(ConveyorBeltComponent, "{61ECBDD3-94B4-4982-96CA-284FA27A8378}");
@@ -35,11 +42,28 @@ namespace ROS2
        //////////////////////////////////////////////////////////////////////////
 
    private:
+
+       // Physics::RigidBodyNotifications overrides...
+       void OnPhysicsEnabled(const AZ::EntityId& entityId) override;
+       void OnPhysicsDisabled(const AZ::EntityId& entityId) override;
+
+       void OnTriggerEnter(const AzPhysics::TriggerEvent& triggerEvent);
+       void OnTriggerExit(const AzPhysics::TriggerEvent& triggerEvent);
+
        // AZ::TickBus::Handler overrides
        void OnTick(float delta, AZ::ScriptTimePoint timePoint) override;
 
+       void PostPhysicsSubTick(float fixedDeltaTime);
+
        float m_speed = 1.0f;
        AZ::EntityId m_TempConveyorEntityId;
+
+
+       AZStd::unordered_map<AZ::EntityId, int> m_entitiesOnBelt;
+
+       AzPhysics::SimulatedBodyEvents::OnTriggerEnter::Handler m_onTriggerEnterHandler;
+       AzPhysics::SimulatedBodyEvents::OnTriggerExit::Handler m_onTriggerExitHandler;
+       AzPhysics::SceneEvents::OnSceneSimulationFinishHandler m_sceneFinishSimHandler;
 
    };
 } // namespace ROS2
